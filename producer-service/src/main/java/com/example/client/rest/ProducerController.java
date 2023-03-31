32074c08f7ad4d44dbcf274b48578cea43c399a4
Admin <Admin@Admin-PC>
2023-03-31 16:38:15 +0530

    second commit

--- producer-service/src/main/java/com/example/client/rest/ProducerController.java
+++ producer-service/src/main/java/com/example/client/rest/ProducerController.java
@@ -2,16 +2,24 @@ package com.example.client.rest;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/producer")
 public class ProducerController {
 	
 	@Value("${server.port}")
 	private String serverHost;
 	
-	@GetMapping("message")
+	@GetMapping("/message")
 	public String message() {
 		return "Hello from Producer on port:"+ serverHost;
+	}
+	
+	@GetMapping("/message/{id}")
+	public String message(@PathVariable int id) {
+		return "Hello from Producer on port:"+ serverHost +" with path parameter ="+ id;
 	}
 }
