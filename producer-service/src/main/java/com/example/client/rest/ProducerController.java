a0dc36cd90920216a752582f8daa886b73066464
Admin <Admin@Admin-PC>
2023-04-09 10:50:34 +0530

    commit for log

--- producer-service/src/main/java/com/example/client/rest/ProducerController.java
+++ producer-service/src/main/java/com/example/client/rest/ProducerController.java
@@ -1,5 +1,7 @@
 package com.example.client.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,17 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/producer")
 public class ProducerController {
-	
+	Logger logger = LoggerFactory.getLogger(ProducerController.class);
 	@Value("${server.port}")
 	private String serverHost;
 	
 	@GetMapping("/message")
 	public String message() {
+		logger.debug("*****Started data from producer using endpoint /message");
 		return "Hello from Producer on port:"+ serverHost;
 	}
 	
 	@GetMapping("/message/{id}")
 	public String message(@PathVariable int id) {
+		logger.debug("*****Started data from producer using endpoint /message/id");
 		return "Hello from Producer on port:"+ serverHost +" with path parameter ="+ id;
 	}
 }
